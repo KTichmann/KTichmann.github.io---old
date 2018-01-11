1f79bbd59df6afc9f5edd9986c492dfd9fe3dd0c
Kerwin Tichmann <ktichmann@gmail.com>
2018-01-11 15:47:10 +0000

    removed navbar, cleaned up code, added comments

--- js/script.js
+++ js/script.js
@@ -1,52 +1,61 @@
 //look for changes on the slider value
 //map the numbers 1-4 to the four different layouts ["minimalist", "creative", "professional", "pretentious"]
 //change the value --> add display: none to all the values, then remove display: none from the chosen valueEl
+
+//For support in IE/Edge and older browsers:
 if (!NodeList.prototype.forEach && Array.prototype.forEach) {
   NodeList.prototype.forEach = Array.prototype.forEach;
 }
-
+//
 function init(){
-  websiteUI.switchStyle(2, websiteUI.styleArr)
-  navBar();
+  websiteUI.switchStyle(2, websiteUI.styleArr);
+  // websiteUI.addAnimation(".portfolioimg", "tada")
+  // navBar();
 }
 //read slider input value...
-var websiteUI = (function(){
-  let styleArr = ["minimalist", "creative", "professional", "pretentious"];
-
-  (function user_choice(){
-    document.getElementById("0").addEventListener("click", function(){switchStyle(0, styleArr)})
-    document.getElementById("1").addEventListener("click", function(){switchStyle(1, styleArr)})
-    document.getElementById("2").addEventListener("click", function(){switchStyle(2, styleArr)})
-    document.getElementById("3").addEventListener("click", function(){switchStyle(3, styleArr)})
-  })();
-    // let inputNum = document.querySelector(".js-ranger-input");
-    // inputNum.addEventListener("input", function(){
-    //   switchStyle(styleArr[inputNum.value -1], styleArr)
-    //   console.log(inputNum.value
-
+const websiteUI = (function(){
+  const styleArr = ["minimalist", "creative", "professional", "pretentious"];
+//IIFE to add event listeners to buttons
+    (function user_choice(){
+      for(let i=0; i < styleArr.length; i++){
+        document.getElementById(i.toString()).addEventListener("click", () => {switchStyle(i, styleArr); setActive("li", i)} );
+      }
+    })();
+  //Takes an array of class-names, and sets every element with these class name to hidden, except the one at index num
   function switchStyle(num, arr){
-    let type = arr[num];
-    arr.map(function(item){
-    document.querySelectorAll(`.${item}`).forEach(node => {node.classList.add("hidden")});
-    })
-    document.querySelectorAll(`.${type}`).forEach(item => {item.classList.remove("hidden")});
-
-    document.querySelectorAll("li").forEach(item => {item.classList.remove("active")});
+    let choice = arr[num];
+    //Set every item to hidden
+    arr.forEach(function(item){
+      document.querySelectorAll(`.${item}`).forEach(node => {node.classList.add("hidden"); node.classList.remove("active")});
+    });
+    //remove hidden tag on all items with selected class
+    document.querySelectorAll(`.${choice}`).forEach(item => {item.classList.remove("hidden")});
+  }
+  //removes active tag from all elements of type "type" (eg. "#id", ".class" etc.), adds active tag to 
+  function setActive(type, num){
+    document.querySelectorAll(type).forEach(item => {item.classList.remove("active")});
     document.getElementById(num).classList.add("active");
-    }
-
+  }
+  function addAnimation(el, animation){
+  	document.querySelectorAll(el).forEach((element) => {
+  	  element.addEventListener("mouseover", () => {element.classList.add("animated", animation) });
+  	  element.addEventListener("mouseleave", () => {element.classList.remove("animated", animation)});
+  	});
+  }
   return{
     styleArr: styleArr,
     switchStyle: switchStyle,
-  }
+    addAnimation: addAnimation
+  };
   })();
 
-function navBar(){
-  document.querySelector("i").addEventListener("click", function(){
-    let nav = document.querySelector(".nav").classList
-    if (nav[1] === "seen"){nav.remove("seen")}
-    else{nav.add("seen")}
-  })
-}
+//handle the navigation bar
+// function navBar(){
+//   document.querySelector("i").addEventListener("click", function(){
+//     let nav = document.querySelector(".nav").classList
+//     if (nav[1] === "seen"){nav.remove("seen")}
+//     else{nav.add("seen")}
+//   })
+// }
 
 init();
